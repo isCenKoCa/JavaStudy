3fc5bd0f2deec820b10aee130948f8ef36f4f044
ickoca <ickoca@gmail.com>
2022-06-19 11:57:07 +0300

    v002

--- src/Day06_WrapperClass/C04.java
+++ src/Day06_WrapperClass/C04.java
@@ -1,2 +1,16 @@
-package Day06_WrapperClass;public class C04 {
+package Day06_WrapperClass;
+
+public class C04 {
+    public static void main(String[] args) {
+
+        int toplam = 0;
+
+        for(int i = 1; i < 100; i++)
+        {
+            toplam += i; // toplam = toplam + i;
+        }
+        System.out.println("Toplam = " + toplam);
+    }
 }
+
+
