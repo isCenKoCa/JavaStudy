84113361f2fbd86a6495490128acd58a8a4231ac
ickoca <ickoca@gmail.com>
2022-06-19 17:19:11 +0300

    ders

---
+++ src/Day06/C05.java
@@ -0,0 +1,15 @@
+package Day06;
+
+import java.util.Scanner;
+
+public class C05 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("isminizi giriniz");
+
+        String isim=scanner.nextLine();
+
+        System.out.println("Adınız ve Soyadınız:"+isim);
+    }
+}
